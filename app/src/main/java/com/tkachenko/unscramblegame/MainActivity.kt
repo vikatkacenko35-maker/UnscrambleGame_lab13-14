a5796848209d2ea452b6041aa5a7b9bb47ed2332
karasidnez <karasdnez@gmail.com>
2026-03-25 15:50:34 +0300

    Step 5: Connect ViewModel to UI

--- app/src/main/java/com/tkachenko/unscramblegame/MainActivity.kt
+++ app/src/main/java/com/tkachenko/unscramblegame/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tkachenko.unscramblegame.ui.theme.UnscrambleGameTheme
+import com.tkachenko.unscramblegame.ui_model.GameScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnscrambleGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    GameScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+                }
+
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnscrambleGameTheme {
-        Greeting("Android")
-    }
-}
